018f59bd726bfdaeb899d44677984ad850f3d1ce
Daniil Sluysarev <mr.dancheak@gmail.com>
2024-03-27 01:07:17 +0300

    CORS policy updated

--- cmd/carshare-api/main.go
+++ cmd/carshare-api/main.go
@@ -9,6 +9,7 @@ import (
 	"carshare-api/internal/storage/pgsql"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"log"
 	"net/http"
 	"os"
@@ -33,11 +34,18 @@ func main() {
 	router := chi.NewRouter()
 	router.Use(middleware.URLFormat)
 	router.Use(middleware.Logger)
-	// CORS
-	router.Use(middleware.SetHeader("Access-Control-Allow-Origin", "http://localhost:3000"))
-	router.Use(middleware.SetHeader("Access-Control-Allow-Credentials", "true"))
-	router.Use(middleware.SetHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"))
-	router.Use(middleware.SetHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"))
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	// Protected routes
 	router.Route("/api", func(r chi.Router) {
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/go-chi/render v1.0.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
